c0caadae314418a21944419a134877cbdccb80d0
joabeplay1 <joabejonatas186@gmail.com>
2026-03-24 19:47:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,86 @@
+{
+  "app": "Karaokê",
+  "version": "1.0",
+  "videos": [
+    {
+      "title": "Thiaguinho - Conquistando o Impossível (Karaokê)",
+      "youtube_url": "https://www.youtube.com/watch?v=nFpjkBHvAT4",
+      "id": "69c30e06045e666d2bf13698",
+      "created_date": "2026-03-24T22:19:50.835000",
+      "updated_date": "2026-03-24T22:19:50.835000",
+      "created_by_id": "699ef646bd1bcac033057358",
+      "created_by": "pikokapikokaplay@gmail.com",
+      "is_sample": false
+    },
+    {
+      "title": "Dilsinho - Recaída (Karaokê)",
+      "youtube_url": "https://www.youtube.com/watch?v=5rFD-rVMrJg",
+      "id": "69c30e06045e666d2bf13699",
+      "created_date": "2026-03-24T22:19:50.835000",
+      "updated_date": "2026-03-24T22:19:50.835000",
+      "created_by_id": "699ef646bd1bcac033057358",
+      "created_by": "pikokapikokaplay@gmail.com",
+      "is_sample": false
+    },
+    {
+      "title": "Dilsinho - Basta Você Me Ligar (Karaokê)",
+      "youtube_url": "https://www.youtube.com/watch?v=6tJyIGFfkZA",
+      "id": "69c30e06045e666d2bf1369a",
+      "created_date": "2026-03-24T22:19:50.835000",
+      "updated_date": "2026-03-24T22:19:50.835000",
+      "created_by_id": "699ef646bd1bcac033057358",
+      "created_by": "pikokapikokaplay@gmail.com",
+      "is_sample": false
+    },
+    {
+      "title": "thales",
+      "youtube_url": "https://youtu.be/TujpUoqZdoE?si=QpULucr2tAgSAlbX",
+      "id": "69c13ba3c5d1f3bd5f96958d",
+      "created_date": "2026-03-23T13:09:55.705000",
+      "updated_date": "2026-03-23T13:09:55.705000",
+      "created_by_id": "699ef646bd1bcac033057358",
+      "created_by": "pikokapikokaplay@gmail.com",
+      "is_sample": false
+    },
+    {
+      "title": "sorriso marato",
+      "youtube_url": "https://youtu.be/33VtV9Gnjyo?si=VuXapf1A3rGoA5OY",
+      "id": "699ef78b9c78d27e9f480782",
+      "created_date": "2026-02-25T13:22:19.318000",
+      "updated_date": "2026-02-25T13:22:19.318000",
+      "created_by_id": "699ef646bd1bcac033057358",
+      "created_by": "pikokapikokaplay@gmail.com",
+      "is_sample": false
+    },
+    {
+      "title": "Evidências - Chitãozinho e Xororó (Karaokê)",
+      "youtube_url": "https://www.youtube.com/watch?v=ePjtnSPFWK8",
+      "id": "699ef6adb0f7d92950818568",
+      "created_date": "2026-02-25T13:18:37.416000",
+      "updated_date": "2026-02-25T13:18:37.416000",
+      "created_by_id": "699ef646bd1bcac033057358",
+      "created_by": "pikokapikokaplay@gmail.com",
+      "is_sample": false
+    },
+    {
+      "title": "Shallow - Lady Gaga ft. Bradley Cooper (Karaokê)",
+      "youtube_url": "https://www.youtube.com/watch?v=bo_efYhYU2A",
+      "id": "699ef6adb0f7d92950818569",
+      "created_date": "2026-02-25T13:18:37.416000",
+      "updated_date": "2026-02-25T13:18:37.416000",
+      "created_by_id": "699ef646bd1bcac033057358",
+      "created_by": "pikokapikokaplay@gmail.com",
+      "is_sample": false
+    },
+    {
+      "title": "Bohemian Rhapsody - Queen (Karaokê)",
+      "youtube_url": "https://www.youtube.com/watch?v=fJ9rUzIMcZQ",
+      "id": "699ef6adb0f7d92950818567",
+      "created_date": "2026-02-25T13:18:37.416000",
+      "updated_date": "2026-02-25T13:18:37.416000",
+      "created_by_id": "699ef646bd1bcac033057358",
+      "created_by": "pikokapikokaplay@gmail.com",
+      "is_sample": false
+    }
+  ]
+}
